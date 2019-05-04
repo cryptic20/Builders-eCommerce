a00eb7114d7b36fc7b53cb801b25a7558ac8eea3
cryptic20 <sherwin.wyco20@gmail.com>
2019-05-03 23:18:02 -0400

    deleted unused file

--- saleor/static/js/components/cube_token.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const CUBE_API_SECRET = process.env.CUBEJS_API_SECRET;
-
-function get_token(){
-  const cubejsToken = jwt.sign({}, CUBE_API_SECRET, {expiresIn: '30d'});
-
-  return cubejsToken;
-}
